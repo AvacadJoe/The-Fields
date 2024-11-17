860181f4c38196e99a83f4eba1bcaa5ea099e23b
joe-ricker <ee@joericker.net>
2024-11-17 13:58:25 -0600

    Updated
    Death  dialogue, light direction loop

--- scenes/world_environment.gd
+++ scenes/world_environment.gd
@@ -7,11 +7,15 @@ extends WorldEnvironment
 @export var ending_light_energy = 0.0
 @export var fog_width = 100
 @export var game_area_radius = 200
+@export var day_time_length = 100
+@export var sunlight_delay = 10
 
 @onready var light = $DirectionalLight3D
 
 func _ready():
     self.environment.volumetric_fog_enabled = true
+    await get_tree().create_timer(120).timeout #120 second delay before starting the sun cycle
+    self.tween_down() 
     pass
 
 func _process(delta):
@@ -27,3 +31,21 @@ func _process(delta):
         self.environment.volumetric_fog_density = starting_fog_density
 
     pass
+    
+
+
+# Sun cycle loop
+func tween_up():
+    await get_tree().create_timer(sunlight_delay).timeout
+    
+    # Spring ghost settings: var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+    # Standard ghost settings: var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+    var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+    tween.tween_property(light, "rotation", Vector3(-70.0, 0.0, 0.0), day_time_length)
+    tween.tween_callback(self.tween_down)    
+
+func tween_down():
+    await get_tree().create_timer(sunlight_delay).timeout
+    var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+    tween.tween_property(light, "rotation", Vector3(-20, 0.0, 0.0), day_time_length)
+    tween.tween_callback(self.tween_up)    
